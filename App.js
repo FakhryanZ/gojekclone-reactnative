9859052ee8cff5f1789a6f406fc2ccb17f6c5731
FakhryanZ <fakhryanmuhammad@gmail.com>
2020-11-04 09:45:15 +0700

    internal information section

--- App.js
+++ App.js
@@ -48,6 +48,8 @@ import bannerFood from './dummy/food-banner.jpg';
 
 import logoWhite from './logo/white.png';
 import logoGoFood from './logo/go-food.png';
+import logoGojek from './logo/gojek.png';
+import facebookConnet from './dummy/facebook-connect.png';
 
 const App = () => {
   return (
@@ -477,6 +479,81 @@ const App = () => {
               </TouchableOpacity>
             </View>
           </View>
+          {/* internal information section */}
+          <View style={{paddingHorizontal: 16}}>
+            <View
+              style={{
+                height: 15,
+                width: 60,
+                marginLeft: -4,
+              }}>
+              <Image
+                source={logoGojek}
+                style={{
+                  height: undefined,
+                  width: undefined,
+                  resizeMode: 'contain',
+                  flex: 1,
+                }}
+              />
+            </View>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 17,
+                color: '#1C1C1C',
+                marginTop: 15,
+                marginBottom: 20,
+              }}>
+              Complete your profile
+            </Text>
+            <View style={{flexDirection: 'row', marginBottom: 16}}>
+              <View>
+                <Image source={facebookConnet} />
+              </View>
+              <View style={{marginLeft: 16, flex: 1}}>
+                <Text
+                  style={{fontSize: 15, color: '#4A4A4A', fontWeight: 'bold'}}>
+                  Connect with Facebook
+                </Text>
+                <Text
+                  style={{
+                    color: '#4A4A4A',
+                    fontSize: 14,
+                    fontWeight: 'normal',
+                    width: '70%',
+                  }}>
+                  Log in faster without verification code
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#61A756',
+                paddingHorizontal: 12,
+                paddingVertical: 11,
+                alignSelf: 'flex-end',
+                borderRadius: 4,
+              }}>
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  textAlign: 'center',
+                }}>
+                CONNECT
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              borderBottomColor: '#E8E9ED',
+              borderBottomWidth: 1,
+              marginBottom: 20,
+              marginTop: 16,
+            }}
+          />
           {/* gofood banner */}
           <View style={{paddingHorizontal: 16}}>
             <View
